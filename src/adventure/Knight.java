800319bf6310d5e8fdf91f89ba9ad09c26d89243
Leole <39447956+Nkhatho@users.noreply.github.com>
2018-10-15 11:48:15 +0200

    Knight class has been set.

---
+++ src/adventure/Knight.java
@@ -0,0 +1,21 @@
+package adventure;
+
+/**
+ *
+ * @author Leolen
+ */
+public class Knight extends Player{
+    
+    public Knight(String name) {
+        super("Sir ".concat(name), 150, 100, 150);         
+        super.setPlayerClass("Knight");
+    }
+    
+    @Override
+    public void attack(Monster target){
+        System.out.printf("%s swings his sword at the %s\n", getName(), target.toString());
+        System.out.printf("%s loses %d\n", target.toString(), getAttackStrength());
+        target.setHp(target.getHp() - getAttackStrength());
+    }
+    
+}
