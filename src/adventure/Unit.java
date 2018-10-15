542f76cd0cb6967ae3d0cefff7a6c77f53c98f7e
Leole <39447956+Nkhatho@users.noreply.github.com>
2018-10-15 10:34:34 +0200

    Unit class has been set.

--- src/adventure/Unit.java
+++ src/adventure/Unit.java
@@ -13,4 +13,52 @@ public abstract class Unit {
     private int hp;
     private int attackStrength;
     private int defence;
+    
+    public Unit(int hp, int attackStrength, int defence){
+        this.hp = hp;
+        this.attackStrength = attackStrength;
+        this.defence = defence;
+    }
+
+    /**
+     * @return the hp
+     */
+    public int getHp() {
+        return hp;
+    }
+
+    /**
+     * @param hp the hp to set
+     */
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    /**
+     * @return the attackStrength
+     */
+    public int getAttackStrength() {
+        return attackStrength;
+    }
+
+    /**
+     * @param attackStrength the attackStrength to set
+     */
+    public void setAttackStrength(int attackStrength) {
+        this.attackStrength = attackStrength;
+    }
+
+    /**
+     * @return the defence
+     */
+    public int getDefence() {
+        return defence;
+    }
+
+    /**
+     * @param defence the defence to set
+     */
+    public void setDefence(int defence) {
+        this.defence = defence;
+    }
 }
