d5352369152e2055e5882b8791cb41df9339e174
Leole <39447956+Nkhatho@users.noreply.github.com>
2018-10-15 11:57:58 +0200

    Rogue class has been set.

---
+++ src/adventure/Rogue.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package adventure;
+
+/**
+ *
+ * @author Leolen
+ */
+public class Rogue extends Player{
+    public Rogue(String name) {
+        super("Sly ".concat(name), 80, 100, 80);
+        super.setPlayerClass("Rogue");
+    }
+    @Override
+    public void attack(Monster target){
+        System.out.printf("%s throws a fireball at the %s\n", 
+                getName(), target.toString());
+        System.out.printf("%s loses %d\n", target.toString(), getAttackStrength());
+        target.setHp(target.getHp() - getAttackStrength());
+    }
+}
